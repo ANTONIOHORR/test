e33cbe221fe35c21e46cc466592a71169e796ec2
ANTONIOHORR <93744361+ANTONIOHORR@users.noreply.github.com>
2021-11-04 19:56:45 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var opa ;
+function setup() {
+  createCanvas(400,400);
+  opa=createSprite(20,200,50,30);
+  console.log("pirulito")
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
